6fdb227cc295c6c83678eff2778af01116f7298e
komplekt17 <komplekt_17@mail.ru>
2019-12-11 16:53:58 +0500

    getingDataUser added

--- backend/routes/users-router.js
+++ backend/routes/users-router.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-let User = require('../models/users-model');
+const User = require('../models/users-model');
+const Section = require('../models/sections-model');
+const Tag = require('../models/tags-model');
+const Note = require('../models/notes-model');
 
-// добавление новой user
+// добавление нового user
 router.route('/create').post((req, res) => {
 	const newUser = new User({
 		login: req.body.login,
@@ -27,6 +30,74 @@ router.route('/create').post((req, res) => {
 				message: 'User not created!'
 			});
 		});
+});
+
+// Войти зарегистрированному пользователю
+// И получение данных, если login и pass существуют
+router.route('/enter').post(async (req, res) => {
+	// Search for a user by login and pass.
+	// Поиск пользователя по электронной почте и паролю.
+	const findByCredentials = async (login, pass) => {
+		let user = {};
+		// { raw: true } - получение результата без метаданных
+		const result = await User.findOne({ raw: true }, { where: { login } });
+
+		if (!result) user = { error: 'Invalid login credentials' };
+		else {
+			user = result;
+
+			// const isPasswordMatch = await bcrypt.compare(pass, user.pass)
+			const isPasswordMatch = pass === user.pass;
+			if (!isPasswordMatch) {
+				user = { error: 'Invalid password credentials' };
+			}
+		}
+		return user;
+	};
+
+	//Login a registered user
+	try {
+		const { login, pass } = req.body;
+		const user = await findByCredentials(login, pass);
+		const { error } = user;
+		if (error) {
+			const data = { error };
+			return res.status(201).send(data);
+		} else {
+			// const token = await user.generateAuthToken();
+			const sections = await Section.findAll(
+				// { raw: true } - получение результата без метаданных
+				{ raw: true },
+				{ where: { userId: user.id } }
+			);
+			const tags = await Tag.findAll(
+				// { raw: true } - получение результата без метаданных
+				{ raw: true },
+				{ where: { userId: user.id } }
+			);
+			const notes = await Note.findAll(
+				// { raw: true } - получение результата без метаданных
+				{ raw: true },
+				{ where: { userId: user.id } }
+			);
+
+			const data = {
+				user,
+				// token,
+				sections,
+				tags,
+				notes
+			};
+			// console.log('data', data);
+
+			return res.status(201).send(data);
+		}
+	} catch (error) {
+		return res.status(400).send({
+			error,
+			message: 'Login is failed'
+		});
+	}
 });
 
 module.exports = router;
