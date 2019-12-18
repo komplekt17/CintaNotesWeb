3c8fed6c4ff57ac905c3514ee5b004e5ee356332
komplekt17 <komplekt_17@mail.ru>
2019-12-18 14:30:24 +0500

    update, logout, reset-pass added

--- backend/routes/users-router.js
+++ backend/routes/users-router.js
@@ -42,6 +42,47 @@ const findByCredentials = async (userLogin, pass) => {
 	return user;
 };
 
+// отправляем новый пароль на email user'a
+const sendNewPassword = async user => {
+	//console.log('string_64', user)
+
+	let mailOptions = {
+		from: process.env.SERVICE_USER,
+		to: user.login,
+		subject: 'Reset your account password',
+		html: `<h4><b>Resetting Password on Fast Pages</b></h4>
+<p>your temporary password - ${user.pass}</p>
+<p>--Fast Pages Team</p>`
+	};
+
+	var transporter = nodemailer.createTransport({
+		host: process.env.SERVICE_HOST,
+		port: 465,
+		secure: true, // true for 465, false for other ports
+		auth: {
+			user: process.env.SERVICE_USER,
+			pass: process.env.SERVICE_PASS
+		}
+	});
+
+	await transporter.sendMail(mailOptions, function(error, info) {
+		if (error) console.log(error);
+		else console.log('Email sent: ' + info.response);
+	});
+};
+
+// генератор случайного пароля
+const getRundomPass = () => {
+	var length = 8,
+		charset =
+			'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+		retVal = '';
+	for (var i = 0, n = charset.length; i < length; ++i) {
+		retVal += charset.charAt(Math.floor(Math.random() * n));
+	}
+	return retVal;
+};
+
 // добавление нового user
 router.route('/create').post(async (req, res) => {
 	const user = new User({
@@ -133,6 +174,96 @@ router.route('/enter').post(async (req, res) => {
 			message: 'Login is failed'
 		});
 	}
+});
+
+// обновление user (изменение пароля)
+router.route('/update/:id').put(async (req, res) => {
+	//console.log(req.body)
+	const user = await User.findOne({ where: { id: req.params.id } });
+	if (!user) {
+		const data = { error, message: 'User not found!' };
+		return res.status(404).json(data);
+	}
+
+	let { inputNewPass, inputOldPass } = req.body;
+	const isPasswordMatch = await bcrypt.compare(inputOldPass, user.pass);
+
+	//console.log(isPasswordMatch);
+
+	if (isPasswordMatch) {
+		user.pass = inputNewPass;
+
+		user
+			.save()
+			.then(() => {
+				const data = {
+					success: true,
+					data: user,
+					message: 'Password was updated successful!'
+				};
+				return res.status(201).json(data);
+			})
+			.catch(error => {
+				const data = { error, message: 'User not updated!' };
+				return res.status(400).json(data);
+			});
+	} else {
+		const data = { error, message: 'Entered incorrect old Password' };
+		return res.status(201).json(data);
+	}
+});
+
+// Выход пользователя из приложения
+router.route('/logout/:token').get(async (req, res) => {
+	// Log user out of the application
+	try {
+		req.user.tokens = req.user.tokens.filter(token => {
+			return token.token != req.token;
+		});
+		await req.user.save();
+		res.send(req.user);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
+// Восстановление пароля пользователя
+router.route('/reset-pass').post(async (req, res) => {
+	const { inputLogin } = req.body;
+	//console.log('string_185', inputLogin)
+
+	User.findOne({ login: inputLogin }, async (error, user) => {
+		if (error) {
+			const data = { error, message: 'User not found!' };
+			return res.status(404).json(data);
+		}
+		if (!user) {
+			const data = {
+				error,
+				message: `Your email ${inputLogin} not registered!`
+			};
+			return res.status(201).json(data);
+		} else {
+			user.pass = await getRundomPass();
+			await sendNewPassword(user);
+
+			user
+				.save()
+				.then(() => {
+					const data = {
+						success: true,
+						//data: user,
+						message: `New Password sent on ${inputLogin}, Check spam`
+					};
+					return res.status(201).json(data);
+				})
+				.catch(error => {
+					const data = { error, message: 'Email not found!' };
+					console.log(error);
+					return res.status(400).json(data);
+				});
+		}
+	});
 });
 
 module.exports = router;
